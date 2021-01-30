6a52a6c2c281e83df8c4caa5a7ea6f2ed40fe2e4
Antoine Tian <antoine186@hotmail.com>
2021-01-30 04:04:19 +0000

    Added UFO torque and switched camera angle

---
+++ Assets/Intro_Rigidbody/Code/Scripts/Torque2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torque2 : MonoBehaviour
+{
+    #region Variables
+    private Rigidbody rb;
+    public float torqueSpeed = 3;
+    #endregion
+
+    #region Built-in Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rb)
+        {
+            rb.AddTorque(Vector3.up * torqueSpeed);
+        }
+    }
+    #endregion
+}
