13eff7705960e26d956583f2c6623a0902ff90e9
Antoine Tian <antoine186@hotmail.com>
2021-01-30 22:56:53 +0000

    Successfully added the Editor scripts

--- Assets/Intro_Heli_Physics/Code/Scripts/Input/Editor/IP_KeyboardHeli_InputEditor.cs
+++ Assets/Intro_Heli_Physics/Code/Scripts/Input/Editor/IP_KeyboardHeli_InputEditor.cs
@@ -8,6 +8,33 @@ namespace IndiePixel
     [CustomEditor(typeof(IP_KeyboardHeli_Input))]
     public class IP_KeyboardHeli_InputEditor : Editor
     {
-        
+        #region Variables
+        IP_KeyboardHeli_Input targetInput;
+        #endregion
+
+        #region Built-in Methods
+        private void OnEnable()
+        {
+            targetInput = (IP_KeyboardHeli_Input)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            DrawDebugUI();
+        }
+        #endregion
+
+        #region Custom Methods
+        void DrawDebugUI()
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField("Throttle: " + targetInput.ThrottleInput);
+            EditorGUILayout.LabelField("Collective: " + targetInput.CollectiveInput);
+            EditorGUILayout.LabelField("Cyclic: " + targetInput.CyclicInput);
+            EditorGUILayout.LabelField("Pedal: " + targetInput.PedalInput);
+            EditorGUILayout.EndVertical();
+        }
+        #endregion
     }
 }
