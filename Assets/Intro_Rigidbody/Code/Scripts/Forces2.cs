5f364c23ac2ac743716c21804fad577782dea163
Antoine Tian <antoine186@hotmail.com>
2021-01-30 03:25:38 +0000

    Added movement to UFO

--- Assets/Intro_Rigidbody/Code/Scripts/Forces2.cs
+++ Assets/Intro_Rigidbody/Code/Scripts/Forces2.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class Forces2 : MonoBehaviour
 {
     #region Variables
+    public float maxSpeed = 1f;
+
+    private Rigidbody rb;
     #endregion
 
     #region Built-in Methods
-    #endregion
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (rb)
+        {
+            rb.AddForce(new Vector3(1f, 0f, 0f) * maxSpeed);
+        }
     }
+    #endregion
 }
