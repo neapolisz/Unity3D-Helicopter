37d3b01c87fb98b49a2780cd3d985787ff7b773a
Antoine Tian <antoine186@hotmail.com>
2021-01-30 19:40:37 +0000

    Added Base RB controller class for modularity first applied to the forces2 script

--- Assets/Intro_Rigidbody/Code/Scripts/Forces2.cs
+++ Assets/Intro_Rigidbody/Code/Scripts/Forces2.cs
@@ -2,21 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Forces2 : MonoBehaviour
+public class Forces2 : IP_Base_RBController
 {
     #region Variables
     public float maxSpeed = 1f;
-
-    private Rigidbody rb;
     #endregion
 
     #region Built-in Methods
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
---
+++ Assets/Intro_Rigidbody/Code/Scripts/IP_Base_RBController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IP_Base_RBController : MonoBehaviour
+{
+    #region Variables
+    protected Rigidbody rb;
+    #endregion
+
+    #region Built-in Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rb)
+        {
+            HandlePhysics();
+        }
+    }
+    #endregion
+
+    #region Custom Methods
+    protected virtual void HandlePhysics()
+    {
+
+    }
+    #endregion
+}
