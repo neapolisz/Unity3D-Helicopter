8094e2ec28aad1c040773ce212c0566d9ffbfba1
Antoine Tian <antoine186@hotmail.com>
2021-01-30 03:15:40 +0000

    Added UFO & Forces script

---
+++ Assets/Intro_Rigidbody/Code/Scripts/Forces2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Forces2 : MonoBehaviour
+{
+    #region Variables
+    #endregion
+
+    #region Built-in Methods
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
