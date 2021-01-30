61c6cc5d1642dd5ca43528564d4564088b10cfe8
Antoine Tian <antoine186@hotmail.com>
2021-01-30 21:18:37 +0000

    Added BaseHeli script unattached

---
+++ Assets/Intro_Heli_Physics/Code/Scripts/Input/IP_BaseHeli_Input.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndiePixel
+{
+    public class IP_BaseInput_Heli : MonoBehaviour
+    {
+        #region Variables
+        [Header("Base Input Properties")]
+        public float vertical = 0f;
+        public float horizontal = 0f;
+        #endregion
+
+        #region Built-in Methods
+        // Update is called once per frame
+        void Update()
+        {
+            HandleInputs();
+        }
+        #endregion
+
+        #region Custom Methods
+        protected virtual void HandleInputs()
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+        }
+        #endregion
+    }
+}
