72389cd6dad67496100b13633108acc95d9e7664
Antoine Tian <antoine186@hotmail.com>
2021-01-30 22:04:32 +0000

    Setting all heli control vars and their properties

---
+++ Assets/Intro_Heli_Physics/Code/Scripts/Input/IP_KeyboardHeli_Input.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndiePixel
+{
+    public class IP_KeyboardHeli_Input : IP_BaseHeli_Input
+    {
+        #region Properties
+        private float throttleInput = 0f;
+        public float ThrottleInput
+        {
+            get { return throttleInput;  }
+        }
+        private float collectiveInput = 0f;
+        public float CollectiveInput
+        {
+            get { return collectiveInput; }
+        }
+        private float cyclicInput = 0f;
+        public float CyclicInput
+        {
+            get { return cyclicInput; }
+        }
+        private float pedalInput = 0f;
+        public float PedalInput
+        {
+            get { return pedalInput; }
+        }
+        #endregion
+
+        #region Built-in Methods
+        #endregion
+
+        #region Custom Methods
+        protected override void HandleInputs()
+        {
+            base.HandleInputs();
+        }
+        #endregion
+    }
+}
